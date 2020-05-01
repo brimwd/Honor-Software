845d3e9921c8c29fb566c63180877de9a0dcfbd8
celloworld <camerondonahue@gmail.com>
2020-05-01 00:11:03 -0700

    nav component

---
+++ honor-software/src/components/SiteNav.vue
@@ -0,0 +1,39 @@
+<template>
+    <nav id="global-nav" class="row"><div class="col-12">
+		<img src="img/honor-logo.png" alt="Honor Software" />
+		<ul>
+			<li v-for="link in links">
+                <a :href="link.path">
+                    {{ link.label }}
+                </a>
+            </li>
+		</ul>
+	</div></nav>
+</template>
+
+<script>
+export default {
+  name: 'SiteNav',
+  props: {
+    links: Array,
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- honor-software/src/views/Home.vue
+++ honor-software/src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SiteNav msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import SiteNav from '@/components/SiteNav.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    SiteNav,
   }
 }
 </script>
