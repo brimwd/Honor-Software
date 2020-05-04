6368653746eac32391ba2735f5f7dcfbe1f6f8a1
celloworld <camerondonahue@gmail.com>
2020-05-04 14:35:22 -0700

    adjusted server base filepath in vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/Honor-Software/' : '/'
+  // publicPath: process.env.NODE_ENV === 'production' ? '/Honor-Software/' : '/'
 }
