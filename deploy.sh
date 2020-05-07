3dfe8de5b7e598b4872ad5ad54381f3504d51da0
celloworld <camerondonahue@gmail.com>
2020-05-07 08:09:45 -0700

    deploy script works

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,15 @@ set -e
 npm run build
 
 cp -f deploy.txt dist/.gitignore
+cp -f CNAME dist/CNAME
+
 cd dist
 
 git init
+git remote add origin https://github.com/brimwd/Honor-Software.git
 git add -A
 git commit -am 'deploy'
 
-git push -f https://github.com/brimwd/Honor-Software.git gh-pages
+git push -f origin master:gh-pages
 
 cd -
