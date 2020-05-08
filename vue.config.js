f61acaff3fa52c9c6be6261c380898133318933a
brimwd <brimwd@gmail.com>
2020-05-08 07:08:27 -0700

    import variables globally

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   // publicPath: process.env.NODE_ENV === 'production' ? '/Honor-Software/' : '/'
+  css: {
+    loaderOptions: {
+      sass: {
+      	prependData: `@import "@/app.scss";`
+	  }
+    }
+  }
 }
