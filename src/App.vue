b3078fae4ec00354f3d02b227e90fe27e8ece99c
brimwd <brimwd@gmail.com>
2020-05-08 07:12:44 -0700

    migrate styles to shared file

--- src/App.vue
+++ src/App.vue
@@ -46,7 +46,6 @@ $headerPlusFooterHeight: calc(#{$headerHeight} + #{$footerHeight});
 $routerViewMinHeight: calc(100vh - #{$headerPlusFooterHeight});
 
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     color: #2c3e50;
