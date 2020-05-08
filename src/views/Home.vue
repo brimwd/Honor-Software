4a21003ea637af5ea40f449a0cedc772e1663d06
brimwd <brimwd@gmail.com>
2020-05-08 07:09:44 -0700

    correct indentation

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -143,62 +143,62 @@ import Partners from '@/components/Partners.vue'
 import PromoBar from '@/components/PromoBar.vue'
 
 export default {
-    name: 'Home',
-    components: {
-        ContactSection,
-        Headline,
-        Marquee,
-        Partners,
-        PromoBar,
-    },
-    data() {
-        return {
-            headlines: [
-                {
-                    title: "News Content",
-                    text: ["Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."],
-                    imageUrl: "",
-                },
-                {
-                    title: "Accelerate your business",
-                    text: ["Automating workflows Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam..."],
-                    imageUrl: "",
-                },
-                {
-                    title: "Better Together",
-                    text: [
-                        "Open-source technologies power incredible projects around the world, from Google's Chrome browser and Android OS to Twitter and a majority of the world's websites.",
-                        "Learn more about how open-source makes the difference in our report.",
-                    ],
-                    imageUrl: "",
-                },
-                {
-                    title: "From conversations to collaborations",
-                    text: ["Our first project starts here"],
-                    imageUrl: "",
-                },
-            ],
-            promos: [
-                {
-                    label: "Latest Updates",
-                    path: "/news/",
-                },
-                {
+  name: 'Home',
+  components: {
+    ContactSection,
+    Headline,
+    Marquee,
+    Partners,
+    PromoBar,
+  },
+  data() {
+    return {
+      headlines: [
+        {
+          title: "News Content",
+          text: ["Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."],
+          imageUrl: "",
+        },
+        {
+          title: "Accelerate your business",
+          text: ["Automating workflows Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam..."],
+          imageUrl: "",
+        },
+        {
+          title: "Better Together",
+          text: [
+            "Open-source technologies power incredible projects around the world, from Google's Chrome browser and Android OS to Twitter and a majority of the world's websites.",
+            "Learn more about how open-source makes the difference in our report.",
+          ],
+          imageUrl: "",
+        },
+        {
+          title: "From conversations to collaborations",
+          text: ["Our first project starts here"],
+          imageUrl: "",
+        },
+      ],
+      promos: [
+        {
+          label: "Latest Updates",
+          path: "/news/",
+        },
+        {
 
-                    label: "Transforming software research",
-                    path: "/stories/scheduleux.html",
-                },
-                {
-                    label: "Learn how",
-                    path: "/news/",
-                },
-                {
-                    label: "Customer Story: Hyperdrive Engineering",
-                    path: "/news/",
-                }
-            ],
-        };
-    },
+          label: "Transforming software research",
+          path: "/stories/scheduleux.html",
+        },
+        {
+          label: "Learn how",
+          path: "/news/",
+        },
+        {
+          label: "Customer Story: Hyperdrive Engineering",
+          path: "/news/",
+        }
+      ],
+    };
+  },
 }
 </script>
 
