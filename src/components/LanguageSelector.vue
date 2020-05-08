540ead8ec67d2dc6bbb67c35434eb9dbb52499ba
brimwd <brimwd@gmail.com>
2020-05-08 07:10:51 -0700

    Update LanguageSelector.vue

--- src/components/LanguageSelector.vue
+++ src/components/LanguageSelector.vue
@@ -5,7 +5,7 @@
   >
     <span>English</span>
     <!-- <span>中文</span> -->
-    <i class="far fa-angle-down" aria-label="language selector" />
+    <i class="far fa-fw fa-angle-down" aria-label="language selector" />
   </div>
 </template>
 
@@ -35,5 +35,23 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.dropdown {
+  border:1px solid transparent;
+  border-radius:4px;
+  cursor:pointer;
+  padding:.5em .75em;
+  transition:all .15s;
+
+  &:hover {border-color:var(--mid-gray)}
+
+  i {
+    @include FA;
+    text-align:center;
+    min-width:1.2em;
+    margin-left:.75em;
+    &::before {content:'\f0ac';}
+  }
+  &:hover i::before {content:'\f107';}
+}
 
 </style>
