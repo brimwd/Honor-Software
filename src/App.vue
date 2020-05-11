4d9e62bdc5a83352281164964f2aa52e1a4b2255
brimwd <brimwd@gmail.com>
2020-05-11 05:43:13 -0700

    normalizes indentation

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
-    <div id="app" class="container-fluid">
-        <Toast :content="toast" />
-        <SiteNav :navLinks="navLinks" />
-        <router-view
-            class="page-content"
-        />
-        <Footer class="footer" />
-    </div>
+  <div id="app" class="container-fluid">
+    <Toast :content="toast" />
+    <SiteNav :navLinks="navLinks" />
+    <router-view
+      class="page-content"
+    />
+    <Footer class="footer" />
+  </div>
 </template>
 
 <script>
@@ -15,28 +15,28 @@ import SiteNav from '@/components/SiteNav.vue'
 import Toast from '@/components/Toast.vue'
 
 export default {
-    components: {
-        Footer,
-        SiteNav,
-        Toast,
-    },
-    data() {
-        return {
-            toast: {
-                message: "Our response to Covid-19",
-                path: "news/covid-19.html",
-            },
-            navLinks: [
-                { label: "News", path: "/news/", },
-                { label: "Customers", path: "/customers/", },
-                { label: "Contact", path: "/contact/", },
-                { label: "About", path: "/about/", },
-                // { label: "Products", path: "/products/", },
-                // { label: "Services", path: "/services/", },
-                // { label: "Resources", path: "/resources/", },
-            ]
-        };
-    },
+  components: {
+    Footer,
+    SiteNav,
+    Toast,
+  },
+  data() {
+    return {
+      toast: {
+        message: "Our response to Covid-19",
+        path: "news/covid-19.html",
+      },
+      navLinks: [
+        { label: "News", path: "/news/", },
+        { label: "Customers", path: "/customers/", },
+        { label: "Contact", path: "/contact/", },
+        { label: "About", path: "/about/", },
+        // { label: "Products", path: "/products/", },
+        // { label: "Services", path: "/services/", },
+        // { label: "Resources", path: "/resources/", },
+      ]
+    };
+  },
 }
 </script>
 
