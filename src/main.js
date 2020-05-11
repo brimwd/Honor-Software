6c02a1cf60144ee1901a22e83b7702139d3c0aac
celloworld <camerondonahue@gmail.com>
2020-05-11 09:03:48 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import { library } from '@fortawesome/fontawesome-svg-core'
+import { dom, library } from '@fortawesome/fontawesome-svg-core'
 import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
@@ -12,6 +12,7 @@ import './app.scss';
 import './app.css';
 
 library.add(faUserSecret)
+dom.watch()
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
