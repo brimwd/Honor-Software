a11d38e5c2b5ab8ad79bde36d895c111eba3f190
celloworld <camerondonahue@gmail.com>
2020-05-12 12:23:45 -0700

    working on route change scroll behavior

--- src/router/index.js
+++ src/router/index.js
@@ -52,11 +52,7 @@ const router = new VueRouter({
   mode: 'history',
   routes,
   scrollBehavior (to, from, savedPosition) {
-    if (savedPosition) {
-      return savedPosition
-    } else {
-      return { x: 0, y: 0 }
-    }
+      return { x: 0, y: 0 };
   }
 })
 
