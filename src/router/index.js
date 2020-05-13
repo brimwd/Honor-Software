5b248ae631a1a94710920cce954bec5d384645f2
brimwd <brimwd@gmail.com>
2020-05-13 04:25:34 -0700

    add Why Honor page to index

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
 import OpenSource from '../views/OpenSource.vue'
+import WhyHonor from '../views/WhyHonor.vue'
 import Contact from '../views/Contact.vue'
 import Customers from '../views/Customers.vue'
 import ScheduleUX from '../views/ScheduleUX.vue'
@@ -33,6 +34,11 @@ const routes = [
         path: '/about/team',
         name: 'Team',
         component: Team
+    },
+    {
+        path: '/about/why-honor',
+        name: 'WhyHonor',
+        component: WhyHonor
     },
     {
         path: '/contact',
