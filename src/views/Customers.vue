736514d23fac397b2c83faca84bf92c5246e6029
brimwd <brimwd@gmail.com>
2020-05-13 03:24:49 -0700

    updates customers content

--- src/views/Customers.vue
+++ src/views/Customers.vue
@@ -3,16 +3,24 @@
 
     <Carousel />
 
-  </article>
+    <div class="row">
+      <div class="col-12 col-md-10 mt-3 mb-5 pb-5 mx-auto">
+        <h3 class="px-3">Honor.Software is proud to support causes including literacy, equality, agriculture, education, economic-empowerment, government, and environmental initiatives.</h3>
+      </div>
+    </div>
+
+  <ContactSection />
+</article>
 </template>
 
 <script>
 import Carousel from '@/components/Carousel.vue'
-
+import ContactSection from '@/components/ContactSection.vue'
 export default {
   name: 'SiteNav',
   components: {
     Carousel,
+    ContactSection,
   },
   // data() {
   //   return {
