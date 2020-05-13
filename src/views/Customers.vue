074095fe218d7c02b63aa3c152b54a86233e54d1
brimwd <brimwd@gmail.com>
2020-05-13 04:25:08 -0700

    code cleanup

--- src/views/Customers.vue
+++ src/views/Customers.vue
@@ -1,15 +1,16 @@
 <template>
-  <article class="customers-page honor-page">
+<article class="customers-page honor-page">
 
-    <Carousel />
+  <Carousel />
 
-    <div class="row">
-      <div class="col-12 col-md-10 mt-3 mb-5 pb-5 mx-auto">
-        <h3 class="px-3">Honor.Software is proud to support causes including literacy, equality, agriculture, education, economic-empowerment, government, and environmental initiatives.</h3>
-      </div>
+  <div class="row">
+    <div class="col-12 col-md-10 mt-3 mb-2 mx-auto">
+      <h3 class="px-3">Honor.Software is proud to support causes including literacy, equality, agriculture, education, economic-empowerment, government, and environmental initiatives.</h3>
     </div>
+  </div>
 
   <ContactSection />
+
 </article>
 </template>
 
