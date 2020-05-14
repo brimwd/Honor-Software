1f6a2f414c456b0eea5316497c13ef0144e69456
brimwd <brimwd@gmail.com>
2020-05-13 19:07:07 -0700

    indentation, consistency

--- src/components/SpeedDial.vue
+++ src/components/SpeedDial.vue
@@ -76,85 +76,85 @@ export default {
   z-index:5;
 
   ul {
-    display: none;
-    margin-bottom: 8px;
+    display:none;
+    margin-bottom:8px;
 
     li {
-        cursor: pointer;
-        list-style: none;
-        margin-left: auto;
-        max-width: -webkit-fit-content;
-        max-width: max-content;
-        opacity: 0;
-        transform: translateY(4px);
-        transition: all 300ms;
-        transition-delay: 100ms;
+      cursor:pointer;
+      list-style:none;
+      margin-left:auto;
+      max-width:-webkit-fit-content;
+      max-width:max-content;
+      opacity:0;
+      transform:translateY(4px);
+      transition:all 300ms;
+      transition-delay:100ms;
 
-        span.label {
-            background-color: var(--bg);
-            border-radius: 4px;
-            display: inline-block;
-            padding: 5px 10px;
+      span.label {
+        background-color:var(--bg);
+        border-radius:4px;
+        display:inline-block;
+        padding:5px 10px;
+      }
+
+      span.icon {
+        background-color:#E8A679;
+        border-radius:30px;
+        color:var(--bg);
+        display:inline-block;
+        font-size:1.55em;
+        margin:6px 2px 6px 6px;
+        padding:6px 8px 3px;
+        vertical-align:middle;
+
+        &.shift-up {
+          padding:5px 8px 4px;
         }
-
-        span.icon {
-            background-color: #E8A679;
-            border-radius: 30px;
-            color: var(--bg);
-            display: inline-block;
-            font-size: 1.55em;
-            margin: 6px 2px 6px 6px;
-            padding: 6px 8px 3px;
-            vertical-align: middle;
-
-            &.shift-up {
-                padding: 5px 8px 4px;
-            }
-        }
+      }
     }
   }
 
 
   #speedDialTrigger {
-    background-color: var(--text);
-    border: 2px solid black;
-    border-color: #ceae74 #bb9b61 #bb9b61 #ceae74;
-    border-radius: 30px;
-    color: var(--bg);
-    cursor: pointer;
-    display: inline-block;
-    font-size: 22px;
-    padding: 11px 12px 7px 13px;
+    background-color:var(--text);
+    border:2px solid black;
+    border-color:#ceae74 #bb9b61 #bb9b61 #ceae74;
+    border-radius:30px;
+    color:var(--bg);
+    cursor:pointer;
+    display:inline-block;
+    font-size:22px;
+    padding:11px 12px 7px 13px;
 
     i {
-        transform: rotateY(0);
-        transition: .2s transform ease-out;
+      transform:rotateY(0);
+      transition:.2s transform ease-out;
     }
 
     &:hover {
-      background-color: var(--bg-intense);
+      background-color:var(--bg-intense);
       @include shadow;
     }
   }
 
   &.open {
-      ul {
-          display: block;
+    ul {
+      display:block;
 
-          li {
-              opacity: 1;
-              transform:translateY(0);
-          }
+      li {
+        opacity:1;
+        transform:translateY(0);
       }
+    }
 
-      #speedDialTrigger {
-          background-color: var(--bg-intense);
-          @include shadow;
+    #speedDialTrigger {
+      background-color:var(--bg-intense);
+      @include shadow;
 
-          i {
-              transform: rotateZ(45deg);
-          }
+      i {
+        transform:rotateZ(45deg);
       }
+    }
   }
 }
 #overlay {
